9b2412a468bfd065e7b323ba65760429efaed1e3
Jonathan Cobb <jonathan@kyuss.org>
2022-09-07 17:59:42 -0400

    use timestamp log instead of simple

--- util/logger.js
+++ util/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston')
 
 const logger = winston.createLogger({
     level: process.env.MOBILETTO_LOG_LEVEL || 'error',
-    format: winston.format.simple(),
+    format: winston.format.timestamp(),
     transports: process.env.MOBILETTO_LOG_FILE
         ? [ new winston.transports.File({ filename: process.env.MOBILETTO_LOG_FILE }) ]
         : [ new winston.transports.Console() ]
