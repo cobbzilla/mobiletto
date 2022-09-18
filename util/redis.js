37266a43981d5d179d38ab47df7b7741b6334d74
Jonathan Cobb <jonathan@kyuss.org>
2022-09-18 10:42:22 -0400

    update redis

--- util/redis.js
+++ util/redis.js
@@ -168,10 +168,7 @@ class MobilettoCache {
         })
     }
 
-    removeMatchingKeys = async (pattern) => {
-        logger.info(`removeMatchingKeys called from ${new Error().stack}`)
-        await this.applyToMatchingKeys(pattern, this.del)
-    }
+    removeMatchingKeys = async pattern => await this.applyToMatchingKeys(pattern, this.del)
 
     flush = async () => {
         this.counters.flush++
