871575cef0858971a0015f034a970714361a4e4a
Jonathan Cobb <jonathan@kyuss.org>
2023-07-14 23:10:51 -0400

    export all drivers as function

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,2 +1,4 @@
 export * from "mobiletto-base";
+import { MobilettoConnectionFunction } from "mobiletto-base";
 export declare const registerDrivers: () => void;
+export declare const allDrivers: () => Record<string, MobilettoConnectionFunction>;
