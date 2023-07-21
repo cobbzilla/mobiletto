59577738d367cc75bb49410c7c06694d8439c1eb
Jonathan Cobb <jonathan@kyuss.org>
2023-07-20 22:15:34 -0400

    upgrade drivers

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,20 @@ import { storageClient as localDriver } from "mobiletto-driver-local";
 import { storageClient as s3Driver } from "mobiletto-driver-s3";
 import { storageClient as b2Driver } from "mobiletto-driver-b2";
 import { storageClient as indexeddbDriver } from "mobiletto-driver-indexeddb";
+import { storageClient as genericDriver } from "mobiletto-driver-generic";
 
 export const registerDrivers = () => {
     registerDriver("local", localDriver as MobilettoDriverParameter);
     registerDriver("s3", s3Driver as MobilettoDriverParameter);
     registerDriver("b2", b2Driver as MobilettoDriverParameter);
     registerDriver("indexeddb", indexeddbDriver as MobilettoDriverParameter);
+    registerDriver("generic", genericDriver as MobilettoDriverParameter);
 };
 
 export const allDrivers = (): Record<string, MobilettoConnectionFunction> => ({
     local: localDriver as MobilettoConnectionFunction,
     s3: s3Driver as MobilettoConnectionFunction,
     b2: b2Driver as MobilettoConnectionFunction,
-    indexeddb: indexeddbDriver as MobilettoConnectionFunction
-})
+    indexeddb: indexeddbDriver as MobilettoConnectionFunction,
+    generic: genericDriver as MobilettoConnectionFunction,
+});
